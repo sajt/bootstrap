7edc7c16fab4162980852230e932043411db325f
Tamás Ámon <tamas.amon@gmail.com>
2015-07-07 15:37:36 +0200

    changed php prefix

--- template.php
+++ template.php
@@ -1,4 +1,4 @@
-﻿<?
+﻿<?php
 // $Id$
 
 /**
@@ -32,11 +32,11 @@ if (is_null(theme_get_setting('bootstrap'))) {
   $settings = theme_get_settings($theme_key);
   // Don't save the toggle_node_info_ variables.
   if (module_exists('node')) {
-    foreach (node_get_types() as $type => $name) {    
+    foreach (node_get_types() as $type => $name) {
       unset($settings['toggle_node_info_' . $type]);
     }
   }
-  
+
   // Save default theme settings.
   variable_set(
     str_replace('/', '_', 'theme_'. $theme_key .'_settings'),
@@ -59,10 +59,10 @@ function bootstrap_preprocess_page(&$vars, $hook) {
   // Update jQuery
   if (arg(0) != 'admin') {
     $scripts = drupal_add_js();
-   
+
     // Use Google for jQuery
     $vars['head'] .= '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>';
-   
+
     unset($scripts['core']['misc/jquery.js']);
     $vars['scripts'] = drupal_get_js('header', $scripts);
   }
@@ -75,7 +75,7 @@ function bootstrap_preprocess_page(&$vars, $hook) {
   unset($css['all']['module']['modules/system/system-menus.css']);
   unset($css['all']['module']['modules/user/user.css']);
   $vars['styles'] = drupal_get_css($css);
-  
+
   // Classes for body element. Allows advanced theming based on context
   // (home page, node of certain type, etc.)
   $classes = split(' ', $vars['body_classes']);
@@ -87,7 +87,7 @@ function bootstrap_preprocess_page(&$vars, $hook) {
     // Add unique class for each page.
     $path = drupal_get_path_alias($_GET['q']);
     $classes[] = bootstrap_id_safe('page-' . $path);
-    
+
     if (arg(0) == 'node') {
       if (arg(1) == 'add') {
         $section = 'node-add';
@@ -97,7 +97,7 @@ function bootstrap_preprocess_page(&$vars, $hook) {
         $section = 'node-' . arg(2);
         $classes[] = bootstrap_id_safe('section-' . $section);
       }
-      
+
     }
 
     // Add unique class for each website section.
@@ -112,27 +112,27 @@ function bootstrap_preprocess_page(&$vars, $hook) {
         }
       }
 
-      $classes[] = bootstrap_id_safe($class); 
+      $classes[] = bootstrap_id_safe($class);
     }
   }
   $vars['body_classes_array'] = $classes;
   $vars['body_classes'] = implode(' ', $classes); // Concatenate with spaces.
-  
-  
-  
+
+
+
   // Theme form variables
   $vars['mobile_breadcrumb_btn_text'] = theme_get_setting('mobile_breadcrumb_btn_text');
   $vars['mobile_offcanvas_btn_text'] = theme_get_setting('mobile_offcanvas_btn_text');
-  
-  
+
+
   // Theme form variables
   $layout_front_offcanvas_sidebar = theme_get_setting('layout_front_offcanvas_sidebar');
-  
+
   $vars['layout_front_display_page_title'] = theme_get_setting('layout_front_display_page_title');
   $vars['layout_front_display_page_content'] = theme_get_setting('layout_front_display_page_content');
   $vars['layout_front_offcanvas_sidebar'] = $layout_front_offcanvas_sidebar;
-  
-  // Create column classes  
+
+  // Create column classes
   if ($vars['left'] && $vars['right'] && $vars['is_front'] && $layout_front_offcanvas_sidebar == TRUE) {
     $vars['css_main'] = ' class="row"';
     $vars['css_content'] = ' class="col-xs-12 col-sm-8 col-md-6 col-lg-6 col-md-push-3 col-lg-push-3"';
@@ -144,7 +144,7 @@ function bootstrap_preprocess_page(&$vars, $hook) {
     $vars['css_right'] = '';
     $vars['css_left'] = ' class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-sm-pull-9 col-md-pull-9 col-lg-pull-9"';
   }
-  
+
   elseif ($vars['left'] && $vars['right']) {
     $vars['css_main'] = ' class="row row-offcanvas row-offcanvas-left"';
     $vars['css_content'] = ' class="col-xs-12 col-sm-8 col-md-6 col-lg-6 col-md-push-3 col-lg-push-3"';
@@ -161,63 +161,63 @@ function bootstrap_preprocess_page(&$vars, $hook) {
     $vars['css_right'] = ' class="col-xs-12 col-sm-3 col-md-3 col-lg-3"';
     $vars['css_left'] = '';
   };
-  
-  
+
+
   // Theme form variables
   $mobile_nav_bg = theme_get_setting('mobile_nav_bg');
   $mobile_logo_use = theme_get_setting('mobile_logo_use');
 	$mobile_logo_path = theme_get_setting('mobile_logo_path');
-  
+
   // Mobile nav bg color override
   if ($mobile_nav_bg != '') {
     $vars['mobile_nav_bg'] = ' style="background-color:' . $mobile_nav_bg . '"';
   }
-  
+
   if ($mobile_logo_use == TRUE) {
     $vars['mobile_logo'] = TRUE;
     $vars['mobile_logo_path'] = $mobile_logo_path;
   }
-  
+
   // Theme form variables
 	$bootstrap_touch_icon_default_path = theme_get_setting('bootstrap_touch_icon_default_path');
 	$bootstrap_touch_icon_default_use = theme_get_setting('bootstrap_touch_icon_default_use');
-  
+
   $bootstrap_touch_icon_ipad_path = theme_get_setting('bootstrap_touch_icon_ipad_path');
 	$bootstrap_touch_icon_ipad_use = theme_get_setting('bootstrap_touch_icon_ipad_use');
-  
+
   $bootstrap_touch_icon_iphone_r_path = theme_get_setting('bootstrap_touch_icon_iphone_r_path');
 	$bootstrap_touch_icon_iphone_r_use = theme_get_setting('bootstrap_touch_icon_iphone_r_use');
-  
+
   $bootstrap_touch_icon_ipad_r_path = theme_get_setting('bootstrap_touch_icon_ipad_r_path');
 	$bootstrap_touch_icon_ipad_r_use = theme_get_setting('bootstrap_touch_icon_ipad_r_use');
-  
+
   $bootstrap_touch_icon_windows_metro_path = theme_get_setting('bootstrap_touch_icon_windows_metro_path');
 	$bootstrap_touch_icon_windows_metro_use = theme_get_setting('bootstrap_touch_icon_windows_metro_use');
 	$bootstrap_touch_icon_windows_metro_color = theme_get_setting('bootstrap_touch_icon_windows_metro_color');
-  
-  
+
+
   // Touch icons
   $bootstrap_touch_icons = '';
-  
+
   if (!empty($bootstrap_touch_icon_default_path) && $bootstrap_touch_icon_default_use == true) {
-    $bootstrap_touch_icons .= '<link rel="apple-touch-icon-precomposed" href="/' . $bootstrap_touch_icon_default_path . '" />';  
+    $bootstrap_touch_icons .= '<link rel="apple-touch-icon-precomposed" href="/' . $bootstrap_touch_icon_default_path . '" />';
   }
   if (!empty($bootstrap_touch_icon_ipad_path) && $bootstrap_touch_icon_ipad_use == true) {
-    $bootstrap_touch_icons .= '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/' . $bootstrap_touch_icon_ipad_path . '" />';  
+    $bootstrap_touch_icons .= '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/' . $bootstrap_touch_icon_ipad_path . '" />';
   }
   if (!empty($bootstrap_touch_icon_iphone_r_path) && $bootstrap_touch_icon_iphone_r_use == true) {
-    $bootstrap_touch_icons .= '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/' . $bootstrap_touch_icon_iphone_r_path . '" />';  
+    $bootstrap_touch_icons .= '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/' . $bootstrap_touch_icon_iphone_r_path . '" />';
   }
   if (!empty($bootstrap_touch_icon_ipad_r_path) && $bootstrap_touch_icon_ipad_r_use == true) {
-    $bootstrap_touch_icons .= '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/' . $bootstrap_touch_icon_ipad_r_path . '" />';  
+    $bootstrap_touch_icons .= '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/' . $bootstrap_touch_icon_ipad_r_path . '" />';
   }
   if (!empty($bootstrap_touch_icon_windows_metro_path) && !empty($bootstrap_touch_icon_windows_metro_color) && $bootstrap_touch_icon_windows_metro_use == true) {
-    $bootstrap_touch_icons .= '<meta name="msapplication-TileImage" content="' . $bootstrap_touch_icon_windows_metro_path . '" />';  
-    $bootstrap_touch_icons .= '<meta name="msapplication-TileColor" content="#' . $bootstrap_touch_icon_windows_metro_color . '" />';  
+    $bootstrap_touch_icons .= '<meta name="msapplication-TileImage" content="' . $bootstrap_touch_icon_windows_metro_path . '" />';
+    $bootstrap_touch_icons .= '<meta name="msapplication-TileColor" content="#' . $bootstrap_touch_icon_windows_metro_color . '" />';
   }
-  
+
   $vars['bootstrap_touch_icons'] = $bootstrap_touch_icons;
-  
+
   $active_trail = menu_get_active_trail();
   end($active_trail);
   $parent = prev($active_trail);
@@ -226,7 +226,7 @@ function bootstrap_preprocess_page(&$vars, $hook) {
   } else {
     $vars['section_title'] = $vars['title'];
   }
-  
+
   bootstrap_contact_buttons($vars);
 }
 
@@ -236,18 +236,18 @@ function bootstrap_preprocess_page(&$vars, $hook) {
  */
 function bootstrap_contact_buttons(&$vars) {
   // Theme form variables
-  
+
   $company_phone = variable_get('company_phone', '');
   $site_mail = variable_get('site_mail', '');
-  
+
   $vars['company_phone'] = $company_phone;
   $vars['site_mail'] = $site_mail;
-  
-  $vars['mobile_contact_btns_icons'] = theme_get_setting('mobile_contact_btns_icons');  
-  
+
+  $vars['mobile_contact_btns_icons'] = theme_get_setting('mobile_contact_btns_icons');
+
   if (!empty($company_phone) || !empty($site_mail)) {
     $mobile_contact_utilities = '<div class="container contact-details visible-xs">';
-    
+
     if (!empty($company_phone)) {
       $mobile_contact_utilities .= '<a href="tel:' . $company_phone . '" class="btn btn-primary btn-xs">' . $company_phone;
       if ($vars['mobile_contact_btns_icons'] == TRUE) {
@@ -255,7 +255,7 @@ function bootstrap_contact_buttons(&$vars) {
       };
       $mobile_contact_utilities .= '</a>';
     };
-    
+
     if (!empty($site_mail)) {
       $mobile_contact_utilities .= '<a href="mailto:' . $site_mail . '" class="btn btn-primary btn-xs">' . $site_mail;
       if ($vars['mobile_contact_btns_icons'] == TRUE) {
@@ -263,10 +263,10 @@ function bootstrap_contact_buttons(&$vars) {
       };
       $mobile_contact_utilities .= '</a>';
     };
-    
+
     $mobile_contact_utilities .= '</div>';
-    
-    
+
+
     $vars['mobile_contact_utilities'] = $mobile_contact_utilities;
     return $vars;
   }
@@ -288,10 +288,10 @@ function bootstrap_preprocess_node(&$vars, $hook) {
  * Override theme_links().
  */
 function bootstrap_links($links, $attributes, $config) {
-  
+
   global $language;
   $output = '';
- 
+
   if (($config == 'primary-links' || $config == 'secondary-links') && count($links) > 0) {
     $output = '<ul class="nav navbar-nav">';
 
@@ -335,13 +335,13 @@ function bootstrap_links($links, $attributes, $config) {
 
     $output .= '</ul>';
   }
-  
+
   elseif ($config == 'button-group' && count($links) > 0) {
     $output = '<div class="btn-group">';
-    
+
     $num_links = count($links);
     $i = 1;
-    
+
     foreach ($links as $key => $link) {
 
       $class = $key;
@@ -380,16 +380,16 @@ function bootstrap_links($links, $attributes, $config) {
 
     $output .= '</div>';
   }
-  
+
   elseif ($config == 'comment-links' && count($links) > 0) {
     $output = '<div class="btn-group btn-group-xs pull-right">';
-    
+
     $num_links = count($links);
     $i = 1;
-    
-    
+
+
     foreach ($links as $key => $link) {
-    
+
     if ($link['title'] == 'delete') {
       $link['attributes']['class'] = 'btn btn-danger';
     }
@@ -407,15 +407,15 @@ function bootstrap_links($links, $attributes, $config) {
 
     $output .= '</div>';
   }
-  
+
   elseif ($attributes['class'] == 'links inline' && count($links) > 0) {
     $output = '<div class="btn-group btn-group-sm">';
-    
+
     $num_links = count($links);
     $i = 1;
-    
+
     foreach ($links as $key => $link) {
-    
+
     if ($link['title'] == 'delete') {
       $link['attributes']['class'] = 'btn btn-danger';
     }
@@ -433,7 +433,7 @@ function bootstrap_links($links, $attributes, $config) {
 
     $output .= '</div>';
   }
-  
+
   elseif (count($links) > 0) {
     $output = '<ul' . drupal_attributes($attributes) . '>';
 
@@ -442,7 +442,7 @@ function bootstrap_links($links, $attributes, $config) {
 
     foreach ($links as $key => $link) {
       $class = $key;
-      
+
 
       // Add first, last and active classes to the list of links to help out themers.
       if ($i == 1) {
@@ -510,7 +510,7 @@ function bootstrap_preprocess_comment(&$vars) {
   $vars['author'] = theme('username', $comment);
   $vars['content'] = $comment->comment;
   $vars['date'] = format_date($comment->timestamp , 'custom', 'jS M Y');
- 
+
   $vars['new'] = $comment->new ? t('new') : '';
   $vars['picture'] = theme_get_setting('toggle_comment_user_picture') ? theme('user_picture', $comment) : '';
   $vars['signature'] = $comment->signature;
@@ -595,8 +595,8 @@ function bootstrap_bootstrap_search_form_wrapper(&$variables) {
   $output .= '</div>';
   return $output;
 }
- 
- 
+
+
 /**
  * Converts a string to a suitable html ID attribute.
  *
@@ -629,20 +629,20 @@ function bootstrap_id_safe($string) {
 function bootstrap_preprocess_block(&$vars, $hook) {
 
   $block = $vars['block'];
-  
+
   // Special classes for blocks.
   $classes = array('block');
   $classes[] = 'block-' . $block->module;
   $classes[] .= block_class($block);
-  
+
   $vars['edit_links_array'] = array();
-  
+
   $vars['edit_links'] = '';
   if (user_access('administer blocks')) {
     include_once drupal_get_path('theme', 'bootstrap') . '/includes/blockediting.inc';
     bootstrap_preprocess_block_editing($vars, $hook);
   }
-  
+
   $vars['classes'] = implode(' ', $classes);
-  
+
 }
